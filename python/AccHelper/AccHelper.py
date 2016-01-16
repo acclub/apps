8e80434e76968a6770fa898846ee2eaf417aa6ee
Surfin Bird <illvdg13@gmail.com>
2016-01-16 15:45:02 +0300

    добавлено топливо

--- python/AccHelper/AccHelper.py
+++ python/AccHelper/AccHelper.py
@@ -4,8 +4,22 @@ try:
 except ImportError:
     pass
 
+# stdlib, SimInfo
+import sys
+import os
+import os.path
+import platform
+if platform.architecture()[0] == "64bit":
+    sysdir=os.path.dirname(__file__)+'/stdlib64'
+else:
+    sysdir=os.path.dirname(__file__)+'/stdlib'
+sys.path.insert(0, sysdir)
+os.environ['PATH'] = os.environ['PATH'] + ";."
+from third_party.sim_info import SimInfo
+
 appWindow = 0
 someLabel = 0
+simInfo = SimInfo()
 
 def acMain(ac_version):
     global appWindow, someLabel
@@ -18,9 +32,23 @@ def acMain(ac_version):
     someLabel = ac.addLabel(appWindow, "")
     ac.setPosition(someLabel, 10, 5)
 
+# AccG, Aero,  BestLap, Brake, CamberDeg, CamberRad, Caster, CGHeight, Clutch, 
+# CurrentTyresCoreTemp, DriftBestLap, DriftLastLap, DriftPoints, DriveTrainSpeed, 
+# DY, DynamicPressure, Gas, Gear, InstantDrift, IsDriftInvalid, IsEngineLimiterOn, 
+# LapCount, LapInvalidated, LapTime, LastFF, LastLap, LastTyresTemp, Load, 
+# LocalAngularVelocity, LocalVelocity, Mz, NdSlip, NormalizedSplinePosition, 
+# PerformanceMeter, RideHeight, RPM, SlipAngle, SlipAngleContactPatch, SlipRatio,
+# SpeedKMH, SpeedMPH, SpeedMS, SpeedTotal, Steer, SuspensionTravel, ToeInDeg,
+# TurboBoost, TyreContactNormal, TyreContactPoint, TyreDirtyLevel,
+# TyreHeadingVector, TyreLoadedRadius, TyreRadius, TyreRightVector, TyreSlip,
+# TyreSurfaceDef, TyreVelocity, Velocity, WheelAngularSpeed, WorldPosition
+
 def acUpdate(delta_t):
-    global someLabel
+    global someLabel, simInfo
     ac.setBackgroundOpacity(appWindow, 0)
     rpm = ac.getCarState(0, acsys.CS.RPM)
-    ac.setText(someLabel, str(round(rpm)))
+    fuel = simInfo.physics.fuel
+    ac.setText(someLabel, str(round(rpm)) + "\n" + str(round(fuel, 1)))
+
+
 
